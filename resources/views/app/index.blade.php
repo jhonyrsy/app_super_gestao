a54bf7d5258a7c024b88448c4754513e150646f1
jhonyrsy <=>
2022-05-10 16:26:31 -0300

    inicio do projeto super_gestao

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentario que será descartado pelo interpretador do blade --}}
+
+@php
+ // Para comentarios de uma linha
+ /*
+  Para comentarios de multiuplas linhas 
+ */
+@endphp
