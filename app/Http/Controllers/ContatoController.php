e7d48ac944469b1323efb064aa3f70f79d5ae587
jhonyrsy <=>
2022-06-22 12:00:54 -0300

    Inicio de validações

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -8,11 +8,22 @@ use App\SiteContatos;
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
-        
-        $contato =  new SiteContatos();
-        
-        $contato->create($request->all());
-       
+
         return view('site.contato', ['titulo'=> 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request) {
+
+         //realizar a validação dos dados do formulário recebidos no request
+         $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+        
+        // SiteContatos::create($request->all());
+
     }
 }
