59dddd40bea74b27ee23dd1449862cc76d5a4c0e
jhonyrsy <=>
2022-06-22 17:48:53 -0300

    Refactoring motivo_contato

--- database/migrations/2022_06_22_202228_create_motivo_contatos_table.php
+++ database/migrations/2022_06_22_202228_create_motivo_contatos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterFornecedoresSoftdelete extends Migration
+class CreateMotivoContatosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class AlterFornecedoresSoftdelete extends Migration
      */
     public function up()
     {
-        Schema::table('fornecedores', function (Blueprint $table) {
-            $table->softDeletes();
-          });
+        Schema::create('motivo_contatos', function (Blueprint $table) {
+            $table->id();
+            $table->string('motivo_contato', 20);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -25,8 +27,6 @@ class AlterFornecedoresSoftdelete extends Migration
      */
     public function down()
     {
-        Schema::table('fornecedores', function (Blueprint $table) {
-            $table->dropSoftDeletes();
-        });
+        Schema::dropIfExists('motivo_contatos');
     }
 }
