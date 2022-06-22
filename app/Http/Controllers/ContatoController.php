fd7a79ba5ce388ab952d05f5fbebe874e9a71e6c
jhonyrsy <=>
2022-06-22 11:41:22 -0300

    Persistindo dados no banco

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContatos;
 
 class ContatoController extends Controller
 {
-    public function contato() {
-         
-        var_dump($_POST);
-        return view('site.contato');
+    public function contato(Request $request) {
+        
+        $contato =  new SiteContatos();
+        
+        $contato->create($request->all());
+       
+        return view('site.contato', ['titulo'=> 'Contato (teste)']);
     }
 }
