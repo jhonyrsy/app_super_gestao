12f62f9108f8e26e9fb395a83acf947576d4621c
jhonyrsy <=>
2022-06-22 14:53:55 -0300

    Ajustando formulario de contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,20 +9,27 @@ class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        return view('site.contato', ['titulo'=> 'Contato (teste)']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
     }
+
 
     public function salvar(Request $request) {
 
-         //realizar a validação dos dados do formulário recebidos no request
-         $request->validate([
-            'nome' => 'required',
+        //realizar a validação dos dados do formulário recebidos no request
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required'
+            'mensagem' => 'required|max:2000'
         ]);
-        
+
         // SiteContatos::create($request->all());
 
     }
